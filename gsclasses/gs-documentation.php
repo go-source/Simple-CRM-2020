39a697af30f5d33aae4df5d76a0e3353155d19f8
Pedro <pedro.bicudo@pmoft.com>
2021-04-09 00:08:47 -0300

    Update gs-documentation.php

--- gsclasses/gs-documentation.php
+++ gsclasses/gs-documentation.php
@@ -97,9 +97,25 @@ NEXT:
 - include admin submenu https://developer.wordpress.org/plugins/administration-menus/sub-menus/
 
 -*- -*- Joomla Component -*- -*-
+framework - tutorial in https://docs.joomla.org/J3.x:Developing_an_MVC_Component/Developing_a_Basic_Component
+1 	helloworld.xml 			this is an XML (manifest) file that tells Joomla! how to install our component.
+2 	assets/helloworld.php 	this is the site entry point to the Hello World! component
+3 	assets/index.html 		prevents web server from listing directory content
+	assets/gsclasses
+	assets/controllers
+	assets/models
+	assets/views
+	assets/js
+	assets/images
+	assets/css
+4 	j4_admin/index.html 		prevents web server from listing directory content
+5 	j4_admin/helloworld.php 	this is the administrator entry point to the Hello World! component
+6 	j4_admin/sql/index.html 	prevents web server from listing directory content
+7 	j4_admin/sql/updates/index.html 		prevents web server from listing directory content
+8 	j4_admin/sql/updates/mysql/index.html 	prevents web server from listing directory content
+9 	j4_admin/sql/updates/mysql/0.0.1.sql 	file allowing to initialise schema version of the com_helloworld component. 
 
-
-
+in GitHub, the folders are precede by j4_ to separate Joomla! and Wordpress folders. Example /j4_site/
 
 
 
